d9e1f7c1db23dba7d616eb0d6fa1db42e30b9d1f
Yukthshree <yukthinaik66@gmail.com>
2020-05-15 08:24:25 +0530

    Updates

---
+++ precache-manifest.dedeb5bfe66da500a697076c475cd5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa4288dfba409530aa2601fbd7e18002",
+    "url": "/Monster-Rolodex.git/index.html"
+  },
+  {
+    "revision": "a0ac2a241eaa73ea892a",
+    "url": "/Monster-Rolodex.git/static/css/main.078f5ff3.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/Monster-Rolodex.git/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Rolodex.git/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ac2a241eaa73ea892a",
+    "url": "/Monster-Rolodex.git/static/js/main.a801bfc8.chunk.js"
+  },
+  {
+    "revision": "479a205a3f79b2280358",
+    "url": "/Monster-Rolodex.git/static/js/runtime-main.e1a53dec.js"
+  }
+]);
